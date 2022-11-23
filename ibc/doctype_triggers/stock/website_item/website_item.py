465fdf0d3af0ecca9ac7945dabb068f4e18ff527
erpcloud.systems <erpcloud.systems@gmail.com>
2022-11-23 11:57:20 +0100

    remove last update on webitem

--- ibc/doctype_triggers/stock/website_item/website_item.py
+++ ibc/doctype_triggers/stock/website_item/website_item.py
@@ -17,11 +17,15 @@ def after_insert(doc, method=None):
 
     ## Get Single Values from Ecs Woocommerce seetings page
     price_list = frappe.db.get_single_value("Ecs Woocommerce", "price_list")
-    woocommerce_user_key = frappe.db.get_single_value("Ecs Woocommerce", "woocommerce_user_key")
+    woocommerce_user_key = frappe.db.get_single_value(
+        "Ecs Woocommerce", "woocommerce_user_key"
+    )
     woocommerce_user_secret = frappe.db.get_single_value(
         "Ecs Woocommerce", "woocommerce_user_secret"
     )
-    woocommerce_create = frappe.db.get_single_value("Ecs Woocommerce", "woocommerce_create")
+    woocommerce_create = frappe.db.get_single_value(
+        "Ecs Woocommerce", "woocommerce_create"
+    )
     system_url = frappe.db.get_single_value("Ecs Woocommerce", "system_url")
 
     ## Get Values From Website Item and Item
@@ -60,20 +64,27 @@ def after_insert(doc, method=None):
 
     categories = []
     categories.append({"id": category_id})
-    if frappe.db.exists("Website Item Group", {"parent": doc.name}, "item_group"):
-        items_groups = frappe.db.get_list("Website Item Group", {"parent": doc.name}, "item_group")
-        for item_group in items_groups:
-            category_ids = frappe.db.get_value(
-                "Item Group", item_group["item_group"], "category_id"
-            )
+    # if frappe.db.exists("Website Item Group", {"parent": doc.name}, "item_group"):
+    #     items_groups = frappe.db.get_list("Website Item Group", {"parent": doc.name}, "item_group")
+    #     for item_group in items_groups:
+    #         category_ids = frappe.db.get_value(
+    #             "Item Group", item_group["item_group"], "category_id"
+    #         )
 
-        categories.append({"id": category_ids})
+    #     categories.append({"id": category_ids})
     data["categories"] = categories
     # frappe.msgprint(json.dumps(data))
     headeroauth = OAuth1(
-        woocommerce_user_key, woocommerce_user_secret, None, None, signature_method="HMAC-SHA1"
+        woocommerce_user_key,
+        woocommerce_user_secret,
+        None,
+        None,
+        signature_method="HMAC-SHA1",
     )
-    headers = {"content-type": "application/json;charset=utf-8", "Content-Length": "376"}
+    headers = {
+        "content-type": "application/json;charset=utf-8",
+        "Content-Length": "376",
+    }
     response = requests.post(
         url=woocommerce_create, data=json.dumps(data), auth=headeroauth, headers=headers
     )
@@ -107,7 +118,9 @@ def validate(doc, method=None):
         woocommerce_user_secret = frappe.db.get_single_value(
             "Ecs Woocommerce", "woocommerce_user_secret"
         )
-        woocommerce_create = frappe.db.get_single_value("Ecs Woocommerce", "woocommerce_create")
+        woocommerce_create = frappe.db.get_single_value(
+            "Ecs Woocommerce", "woocommerce_create"
+        )
         system_url = frappe.db.get_single_value("Ecs Woocommerce", "system_url")
 
         ## Get Values From Website Item and Item
@@ -116,7 +129,9 @@ def validate(doc, method=None):
         # permalink = "https://example.com/product" + doc.web_item_name
         image = system_url + doc.website_image
         price = frappe.db.get_value(
-            "Item Price", {"item_code": sku, "price_list": price_list}, ["price_list_rate"]
+            "Item Price",
+            {"item_code": sku, "price_list": price_list},
+            ["price_list_rate"],
         )
         category_id = frappe.db.get_value("Item Group", doc.item_group, "category_id")
         if not category_id:
@@ -144,25 +159,32 @@ def validate(doc, method=None):
 
         categories = []
         categories.append({"id": category_id})
-        if frappe.db.exists("Website Item Group", {"parent": doc.name}, "item_group"):
-            items_groups = frappe.db.get_list(
-                "Website Item Group", {"parent": doc.name}, "item_group"
-            )
-            for item_group in items_groups:
-                category_ids = frappe.db.get_value(
-                    "Item Group", item_group["item_group"], "category_id"
-                )
+        # if frappe.db.exists("Website Item Group", {"parent": doc.name}, "item_group"):
+        #     items_groups = frappe.db.get_list(
+        #         "Website Item Group", {"parent": doc.name}, "item_group"
+        #     )
+        #     for item_group in items_groups:
+        #         category_ids = frappe.db.get_value(
+        #             "Item Group", item_group["item_group"], "category_id"
+        #         )
 
-                categories.append({"id": category_ids})
+        #         categories.append({"id": category_ids})
         data["categories"] = categories
-        
+
         woocommerce_id = doc.woocommerce_id
         # frappe.msgprint(json.dumps(data))
 
         headeroauth = OAuth1(
-            woocommerce_user_key, woocommerce_user_secret, None, None, signature_method="HMAC-SHA1"
+            woocommerce_user_key,
+            woocommerce_user_secret,
+            None,
+            None,
+            signature_method="HMAC-SHA1",
         )
-        headers = {"content-type": "application/json;charset=utf-8", "Content-Length": "376"}
+        headers = {
+            "content-type": "application/json;charset=utf-8",
+            "Content-Length": "376",
+        }
         response = requests.put(
             url=woocommerce_create + str(woocommerce_id),
             data=json.dumps(data),
