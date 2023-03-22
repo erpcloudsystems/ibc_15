9c3e059335f0faf461df5105294892d6af3b4d09
erpcloud.systems <erpcloud.systems@gmail.com>
2023-03-22 06:21:54 +0100

    export 22-3-2023

--- ibc/ibc/report/brand_added_value_report/brand_added_value_report.py
+++ ibc/ibc/report/brand_added_value_report/brand_added_value_report.py
@@ -19,6 +19,106 @@ def get_data(filters):
 	purchase_in_qty_value = 0
 #  or name = 'Farfisa' or name = 'Honeywe'
 	result = []
+	item_results = frappe.db.sql(f"""
+			select
+				`tabBrand`.name as brand,
+				ifnull(`tabItem`.name,0) as item_code,
+				ifnull(`tabItem`.item_name,0) as item_name,
+				ifnull(`tabItem`.valuation_rate,0) as value,
+				ifnull(`tabItem`.item_group,0) as item_group,
+				ifnull((select sum(amount)
+					from `tabPurchase Invoice Item`
+					join `tabPurchase Invoice` on `tabPurchase Invoice`.name = `tabPurchase Invoice Item`.parent
+					where `tabPurchase Invoice Item`.brand = `tabBrand`.name
+					and `tabPurchase Invoice`.posting_date between '{from_date}' and '{to_date}'
+					and `tabPurchase Invoice`.docstatus = 1), 0) as purchase,
+				ifnull((select sum(amount)
+					from `tabSales Invoice Item`
+					join `tabSales Invoice` on `tabSales Invoice`.name = `tabSales Invoice Item`.parent
+					where `tabSales Invoice Item`.brand = `tabBrand`.name
+					and `tabSales Invoice`.posting_date between '{from_date}' and '{to_date}'
+					and `tabSales Invoice`.docstatus = 1), 0) as sales
+			from `tabBrand`
+			left join `tabItem` on `tabItem`.brand = `tabBrand`.name
+		""",as_dict = 1)
+
+	for item_dict in item_results:
+		data = {
+					'brand': item_dict.brand,
+					'purchase' : item_dict.purchase,
+					'sales_value' : item_dict.sales,
+
+				}
+		frat = 0
+		s = 0
+		current_value = 0
+		sales_value = 0
+		cogs = 0
+		added_value = 0
+		item = item_dict.item_code
+		brand = item_dict.brand
+		warehouses = frappe.db.sql("""select name as name from `tabWarehouse` """, as_dict=1)
+		for warehouse in warehouses:
+			warehousee = warehouse.name
+			opening = frappe.db.sql("""select
+								qty_after_transaction as res,
+								valuation_rate as frate,
+								stock_value as sv
+								from `tabStock Ledger Entry` join `tabWarehouse` on `tabStock Ledger Entry`.warehouse = `tabWarehouse`.name
+								where
+								`tabStock Ledger Entry`.item_code = %s
+								and `tabStock Ledger Entry`.warehouse = %s
+								and `tabStock Ledger Entry`.posting_date <= %s
+								and `tabStock Ledger Entry`.is_cancelled = 0
+								ORDER BY `tabStock Ledger Entry`.posting_date DESC, `tabStock Ledger Entry`.posting_time DESC , `tabStock Ledger Entry`.creation DESC LIMIT 1""",
+											(item, warehousee, from_date), as_dict=1)
+			for tqty in opening:
+				s += tqty.frate
+				frat += tqty.res*tqty.frate
+
+			balance = frappe.db.sql("""select
+											qty_after_transaction as res,
+											valuation_rate as frate
+											from `tabStock Ledger Entry` join `tabWarehouse` on `tabStock Ledger Entry`.warehouse = `tabWarehouse`.name
+											where
+											`tabStock Ledger Entry`.item_code = %s
+											and `tabStock Ledger Entry`.warehouse = %s
+											and `tabStock Ledger Entry`.posting_date <= %s
+											and `tabStock Ledger Entry`.is_cancelled = 0
+											ORDER BY `tabStock Ledger Entry`.posting_date DESC, `tabStock Ledger Entry`.posting_time DESC , `tabStock Ledger Entry`.creation DESC LIMIT 1""",
+										(item, warehousee, to_date), as_dict=1)
+			for tqty in balance:
+				current_value += tqty.res * tqty.frate
+
+			sales_value = item_dict.sales
+			purchase = item_dict.purchase
+			cogs = (purchase + frat) - current_value
+			added_value = (sales_value) - ((purchase + frat) - current_value)
+
+			if sales_value != 0 and cogs != 0:
+				data['opening'] = round(frat,2)
+				data['total_in'] = round(purchase + frat,2)
+				data['current_value'] = round(current_value,2)
+				data['cogs'] = round(cogs,2)
+				# data['sales_value'] = round(sales_value,2)
+				data['added_value'] = round(added_value,2)
+				data['cogs/sales_value'] = str(round((cogs / sales_value),2)*100)  + ' %'
+				data['added_value/sales_value'] = str(round((added_value / sales_value),2) *100) + ' %'
+				data['added_value/cogs'] = str(round((added_value / cogs),2)*100) + ' %'
+			else:
+				data['opening'] = round(frat,2)
+				data['total_in'] = round(purchase + frat,2)
+				data['current_value'] = round(current_value,2)
+				data['cogs'] = round(cogs,2)
+				# data['sales_value'] = round(sales_value,2)
+				data['added_value'] = round(added_value,2)
+				data['cogs/sales_value'] = 0
+				data['added_value/sales_value'] = 0
+				data['added_value/cogs'] = 0
+
+			result.append(data)
+
+	return result
 	brand = frappe.db.sql("""
 		select name as name
 		from `tabBrand`
@@ -140,18 +240,30 @@ def get_data(filters):
 					# frappe.throw(str(sales_value))z
 			sales_value = item_dict.sales
 			purchase = item_dict.purchase
-
-			cogs = (purchase + frat) - current_value
-			added_value = (sales_value) - ((purchase + frat) - current_value)
-			data['opening'] = round(frat,2)
-			data['total_in'] = round(purchase + frat,2)
-			data['current_value'] = round(current_value,2)
-			data['cogs'] = round(cogs,2)
-			# data['sales_value'] = round(sales_value,2)
-			data['added_value'] = round(added_value,2)
-			data['cogs/sales_value'] = str(round((cogs / sales_value),2)*100)  + ' %'
-			data['added_value/sales_value'] = str(round((added_value / sales_value),2) *100) + ' %'
-			data['added_value/cogs'] = str(round((added_value / cogs),2)*100) + ' %'
+			if sales_value != 0 and cogs != 0:
+				cogs = (purchase + frat) - current_value
+				added_value = (sales_value) - ((purchase + frat) - current_value)
+				data['opening'] = round(frat,2)
+				data['total_in'] = round(purchase + frat,2)
+				data['current_value'] = round(current_value,2)
+				data['cogs'] = round(cogs,2)
+				# data['sales_value'] = round(sales_value,2)
+				data['added_value'] = round(added_value,2)
+				data['cogs/sales_value'] = str(round((cogs / sales_value),2)*100)  + ' %'
+				data['added_value/sales_value'] = str(round((added_value / sales_value),2) *100) + ' %'
+				data['added_value/cogs'] = str(round((added_value / cogs),2)*100) + ' %'
+			else:
+				cogs = (purchase + frat) - current_value
+				added_value = (sales_value) - ((purchase + frat) - current_value)
+				data['opening'] = round(frat,2)
+				data['total_in'] = round(purchase + frat,2)
+				data['current_value'] = round(current_value,2)
+				data['cogs'] = round(cogs,2)
+				# data['sales_value'] = round(sales_value,2)
+				data['added_value'] = round(added_value,2)
+				data['cogs/sales_value'] = 0
+				data['added_value/sales_value'] = 0
+				data['added_value/cogs'] = 0
 
 			result.append(data)
 
