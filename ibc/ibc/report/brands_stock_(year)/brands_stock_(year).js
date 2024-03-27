0e5c5c6a6d6ebfd400fa5c3b85d14fe529832e04
Mghoneem10 <mghoneem10@gmail.com>
2024-03-27 15:12:53 +0200

    Report Brand Stock (Year) 27/3/2024 3:12 PM

---
+++ ibc/ibc/report/brands_stock_(year)/brands_stock_(year).js
@@ -0,0 +1,7 @@
+// Copyright (c) 2024, erpcloud.systems and contributors
+// For license information, please see license.txt
+/* eslint-disable */
+
+frappe.query_reports["Brands Stock (year)"] = {
+
+}
