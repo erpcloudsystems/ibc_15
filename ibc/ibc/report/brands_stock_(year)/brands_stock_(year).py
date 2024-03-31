b5e5959158b4eb50aad4d6da14c2f3aa82b1cf65
Mghoneem10 <mghoneem10@gmail.com>
2024-03-31 15:10:47 +0200

    Edit Report Brand Stock 31/3/2024 3:10 PM

--- ibc/ibc/report/brands_stock_(year)/brands_stock_(year).py
+++ ibc/ibc/report/brands_stock_(year)/brands_stock_(year).py
@@ -13,51 +13,27 @@ def execute(filters=None):
     data = get_data()
     return columns, data
 def get_columns():
-        return [
+    columns=[
             {
                 "label": "Brand",
                 "fieldname": "brand",
                 "fieldtype": "Link",
                 "options": "Brand",
                 "width": 150
-            },
-            {
-                "label": "2019",
-                "fieldname": "2019",
+            },]
+    
+    fiscal_years = frappe.get_all("Fiscal Year", fields=["name"], order_by="name ASC")
+    for row in fiscal_years :
+        columns.append(
+               {
+                "label": row.name,
+                "fieldname": row.name,
                 "fieldtype": "Currency",
                 "width": 150
-            },
-            {
-                "label":"2020",
-                "fieldname": "2020",
-                "fieldtype": "Currency",
-                "width": 150
-            },
-            {
-                "label": "2021",
-                "fieldname": "2021",
-                "fieldtype": "Currency",
-                "width": 150
-            },
-            {
-                "label": "2022",
-                "fieldname": "2022",
-                "fieldtype": "Currency",
-                "width": 150
-            },
-            {
-                "label": "2023",
-                "fieldname": "2023",
-                "fieldtype": "Currency",
-                "width": 150
-            },
-            {
-                "label": "2024",
-                "fieldname": "2024",
-                "fieldtype": "Currency",
-                "width": 150
-            },
-		    ]
+            }, 
+        )
+        
+    return columns
     
 def get_data():
     item_results = frappe.db.sql("""
@@ -76,18 +52,19 @@ def get_data():
             brands = item_dict.get('name')
             query = frappe.db.sql(f"""SELECT
                                         IFNULL(SUM(stock_value_difference), 0) AS res,
-                                        `tabFiscal Year`.name AS year
+                                        `tabStock Ledger Entry`.fiscal_year AS year
                                         
                                     FROM
                                         `tabStock Ledger Entry` 
-                                    LEFT JOIN `tabFiscal Year` ON `tabStock Ledger Entry`.fiscal_year = `tabFiscal Year`.name
                                     WHERE
                                         `tabStock Ledger Entry`.brand = '{brands}'
-                                        # AND `tabStock Ledger Entry`.YEAR(posting_date)  < year
+                                        # AND `tabStock Ledger Entry`.fiscal_year  < year
                                         AND `tabStock Ledger Entry`.is_cancelled = 0
                                     GROUP BY
-                                        `tabFiscal Year`.name""", as_dict=True)
+                                        year""", as_dict=True)
+            total = 0.0
             for row in query:
-                data[f"{row.get('year')}"] = row.get('res')
+                total += row.get('res')
+                data[f"{row.get('year')}"] = total
             result.append(data)
     return result
