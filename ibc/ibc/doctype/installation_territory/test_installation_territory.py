8bbf85980224b50eb56246d8f9ab95987ff5fc60
Mghoneem10 <mghoneem10@gmail.com>
2024-04-15 16:42:53 +0200

    installation territory

---
+++ ibc/ibc/doctype/installation_territory/test_installation_territory.py
@@ -0,0 +1,9 @@
+# Copyright (c) 2024, erpcloud.systems and Contributors
+# See license.txt
+
+# import frappe
+from frappe.tests.utils import FrappeTestCase
+
+
+class TestInstallationTerritory(FrappeTestCase):
+	pass
