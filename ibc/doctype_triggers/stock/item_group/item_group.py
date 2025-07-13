e9c5cb46c364cf7dd4ab099297069338bb9d6f00
Mghoneem10 <Mghoneem10@gmail.com>
2025-07-14 00:56:30 +0200

    Edit in item group code

--- ibc/doctype_triggers/stock/item_group/item_group.py
+++ ibc/doctype_triggers/stock/item_group/item_group.py
@@ -88,8 +88,12 @@ def validate(doc, method=None):
         signature_method="HMAC-SHA1"
     )
     headers = {
-        "content-type": "application/json"
-    }
+        "content-type": "application/json;charset=utf-8",
+        "Content-Length": "376",
+        "Connection": "keep-alive",
+        "Accept-Encoding":"gzip, deflate, br",
+        "Accept":"*/*",
+        "User-Agent":"PostmanRuntime/7.42.0"    }
 
     # Data to send
     data = {"name": doc.name}
