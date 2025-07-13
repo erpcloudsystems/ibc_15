9177022c220ee96bf21c7322c3ce724f2287900e
Mghoneem10 <Mghoneem10@gmail.com>
2025-07-14 00:20:51 +0200

    Edit in item group code

--- ibc/doctype_triggers/stock/item_group/item_group.py
+++ ibc/doctype_triggers/stock/item_group/item_group.py
@@ -44,10 +44,6 @@ def after_insert(doc, method=None):
     headers = {
         "content-type": "application/json;charset=utf-8",
         "Content-Length": "376",
-        "Connection": "keep-alive",
-        "Accept-Encoding":"gzip, deflate, br",
-        "Accept":"*/*",
-        "User-Agent":"PostmanRuntime/7.42.0"
     }
     response = requests.post(
         url=woocommerce_create_category,
@@ -55,6 +51,7 @@ def after_insert(doc, method=None):
         auth=headeroauth,
         headers=headers,
     )
+    # frappe.msgprint(response.content)
     frappe.msgprint(response.content)
 
     returned_data = json.loads(response.content)
@@ -76,56 +73,84 @@ def before_validate(doc, method=None):
 
 @frappe.whitelist()
 def validate(doc, method=None):
-    ## Get Single Values from Ecs Woocommerce seetings page
-    woocommerce_user_key = frappe.db.get_single_value(
-        "Ecs Woocommerce", "woocommerce_user_key"
-    )
-    woocommerce_user_secret = frappe.db.get_single_value(
-        "Ecs Woocommerce", "woocommerce_user_secret"
-    )
-    woocommerce_create_category = frappe.db.get_single_value(
-        "Ecs Woocommerce", "woocommerce_create_category"
-    )
-
-    ## Create Data Structure
-    data = {}
-    data["name"] = doc.name
-    if doc.parent_item_group:
-        parent_category = frappe.db.get_value(
-            "Item Group", {"name": doc.parent_item_group}, "category_id"
+    if doc.category_id:
+        ## Get Single Values from Ecs Woocommerce seetings page
+        woocommerce_user_key = frappe.db.get_single_value(
+            "Ecs Woocommerce", "woocommerce_user_key"
         )
-        data["parent"] = parent_category
+        woocommerce_user_secret = frappe.db.get_single_value(
+            "Ecs Woocommerce", "woocommerce_user_secret"
+        )
+        woocommerce_create_category = frappe.db.get_single_value(
+            "Ecs Woocommerce", "woocommerce_create_category"
+        )
+        category_id = doc.category_id
+        new_name = doc.name
+        ## Create Data Structure
+        data = {}
+        data["name"] = new_name
+        if doc.parent_item_group:
+            parent_category = frappe.db.get_value(
+                "Item Group", {"name": doc.parent_item_group}, "category_id"
+            )
+            data["parent"] = parent_category
+        # frappe.msgprint(json.dumps(data))
 
-    # frappe.msgprint(json.dumps(data))
+        headeroauth = OAuth1(
+            woocommerce_user_key,
+            woocommerce_user_secret,
+            None,
+            None,
+            signature_method="HMAC-SHA1",
+        )
+        headers = {
+            "content-type": "application/json;charset=utf-8",
+            "Content-Length": "376",
+        }
+        response = requests.post(
+            url=woocommerce_create_category + str(category_id),
+            data=json.dumps(data),
+            auth=headeroauth,
+            headers=headers,
+        )
+        # frappe.msgprint(response.content)
+    else:
+        data = {}
+        data["name"] = doc.name
+        if doc.parent_item_group:
+            parent_category = frappe.db.get_value(
+                "Item Group", {"name": doc.parent_item_group}, "category_id"
+            )
+            data["parent"] = parent_category
 
-    headeroauth = OAuth1(
-        woocommerce_user_key,
-        woocommerce_user_secret,
-        None,
-        None,
-        signature_method="HMAC-SHA1",
-    )
-    headers = {
-        "content-type": "application/json;charset=utf-8",
-        "Content-Length": "376",
-        "Connection": "keep-alive",
-        "Accept-Encoding":"gzip, deflate, br",
-        "Accept":"*/*",
-        "User-Agent":"PostmanRuntime/7.42.0"
-    }
-    response = requests.post(
-        url=woocommerce_create_category,
-        data=json.dumps(data),
-        auth=headeroauth,
-        headers=headers,
-    )
-    frappe.msgprint(response.content)
+        # frappe.msgprint(json.dumps(data))
 
-    returned_data = json.loads(response.content)
-    
-    doc.category_id = returned_data["id"]
-    doc.save()
-    doc.reload()
+        headeroauth = OAuth1(
+            woocommerce_user_key,
+            woocommerce_user_secret,
+            None,
+            None,
+            signature_method="HMAC-SHA1",
+        )
+        headers = {
+            "content-type": "application/json;charset=utf-8",
+            "Content-Length": "376",
+        }
+        response = requests.post(
+            url=woocommerce_create_category,
+            data=json.dumps(data),
+            auth=headeroauth,
+            headers=headers,
+        )
+        # frappe.msgprint(response.content)
+        frappe.msgprint(response.content)
+
+        returned_data = json.loads(response.content)
+        
+        doc.category_id = returned_data["id"]
+        doc.save()
+        doc.reload()
+
 
 
 @frappe.whitelist()
@@ -180,5 +205,3 @@ def after_rename(first, name, before_rename, after_rename, bool_arg):
         auth=headeroauth,
         headers=headers,
     )
-    # frappe.msgprint(response.content)
-
